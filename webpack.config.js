a5ab8742564110d9d1fbdf8c216a12524a988109
trupu <strubilowicz@zsp5kozuchow.pl>
2019-08-31 18:01:14 +0200

    Basic webpack development configuration

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const {resolve, join} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {};
+
+config.entry = {
+    app: './src/js/app.js'
+}
+
+config.devServer = {
+    port: 9000,
+    compress: true,
+    contentBase: join(__dirname, 'dist')
+}
+
+config.output = {
+    path: resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+}
+
+config.module = {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader','sass-loader']
+            })
+        }
+    ]
+}
+
+config.plugins = [
+    new ExtractTextPlugin('main.css'),
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+]
+
+module.exports = config;
