28fc7776e94b7de30bbd711f4435c440b056e84d
trupu <strubilowicz@zsp5kozuchow.pl>
2019-09-05 15:55:50 +0200

    Finished selector appearance

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,19 @@
 import '../css/app.scss';
-import showSelector from './mixins/selector/selector';
+import Selector from './mixins/selector/selector';
 import table from './mixins/tableGenerator';
 
 const selector = document.querySelector('.cs-select');
+const shadowBox = document.querySelector('.selector-shadow-box');
 
-selector.addEventListener('click', showSelector);
+selector.addEventListener('click', Selector.showSelector);
+shadowBox.addEventListener('click', Selector.hideSelector);
+
+window.onkeydown = function( event ) {
+    if ( event.keyCode == 27 ) {
+        const active = document.querySelector('.cs-active');
+        if (active) Selector.hideSelector();
+    }
+};
+
 
 table();
--- src/js/mixins/selector/selector.js
+++ src/js/mixins/selector/selector.js
@@ -1,7 +1,21 @@
+// Showing list with loaded contant fe. classes
 const showSelector = () => {
-    const ev = event.target.closest('.cs-select');
-    event.target.closest('.selector-title').innerHTML = '';
+    const ev = document.querySelector('.cs-select');
     ev.classList.add('cs-active');
+    const shadowBox = document.querySelector('.selector-shadow-box');
+    shadowBox.style.animation = 'showShadowBox .5s both ease-in-out';
+    const title = document.querySelector('.selector-title');
+    title.style.display = 'none';
 }
 
-export default showSelector;
+// Hiding list with loaded content fe. classes
+const hideSelector = () => {
+    const ev = document.querySelector('.cs-active');
+    ev.classList.remove('cs-active');
+    const shadowBox = document.querySelector('.selector-shadow-box');
+    shadowBox.style.animation = 'showShadowBox .5s both ease-in-out reverse';
+    const title = document.querySelector('.selector-title');
+    title.style.display = 'flex';
+}
+
+export default { showSelector, hideSelector };
